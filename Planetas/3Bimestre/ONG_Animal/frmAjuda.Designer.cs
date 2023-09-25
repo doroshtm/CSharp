ebc02a3d384c3313db3fd93dc627f3d1eaa82a27
ra2257006 <gabriel.c.salzedas@unesp.br>
2023-09-25 10:26:10 -0300

    ONG animal

---
+++ Planetas/3Bimestre/ONG_Animal/frmAjuda.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace _3Bimestre.ONG_Animal
+{
+    partial class frmAjuda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_info = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_info
+            // 
+            this.lbl_info.AutoSize = true;
+            this.lbl_info.Font = new System.Drawing.Font("Microsoft Sans Serif", 58.25F);
+            this.lbl_info.Location = new System.Drawing.Point(12, 66);
+            this.lbl_info.Name = "lbl_info";
+            this.lbl_info.Size = new System.Drawing.Size(783, 178);
+            this.lbl_info.TabIndex = 0;
+            this.lbl_info.Text = "INFORMAÇÕES \r\nSOBRE O PROJETO";
+            this.lbl_info.Click += new System.EventHandler(this.lbl_info_Click);
+            // 
+            // frmAjuda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_info);
+            this.Name = "frmAjuda";
+            this.Text = "frmAjuda";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_info;
+    }
+}
---
+++ Planetas/3Bimestre/ONG_Animal/frmAjuda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3Bimestre.ONG_Animal
+{
+    public partial class frmAjuda : Form
+    {
+        public frmAjuda()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_info_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
